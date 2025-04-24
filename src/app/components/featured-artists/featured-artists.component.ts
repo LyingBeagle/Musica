f8d5a6f7e94c1328f43457ec9c93118733f09e78
LyingBeagle <alonso.ab.diaz@gmail.com>
2025-04-24 19:56:57 -0400

    Seccion de artistas recomendos completada

--- src/app/components/featured-artists/featured-artists.component.ts
+++ src/app/components/featured-artists/featured-artists.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-featured-artists',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./featured-artists.component.scss'],
   standalone:false
 })
-export class FeaturedArtistsComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class FeaturedArtistsComponent {
+  @Input() artists: { name: string; image: string }[] = [
+    { name: '31 Minutos',         image: 'assets/bands/band1.jpg' },
+    { name: 'Leo Rey',            image: 'assets/bands/band2.jpg' },
+    { name: 'Los Picantes',       image: 'assets/bands/band3.jpg' },
+    { name: 'Chancho en Piedra',  image: 'assets/bands/band4.jpg' },
+    { name: 'PSY',                image: 'assets/bands/band5.jpg' },
+  ];
 }
