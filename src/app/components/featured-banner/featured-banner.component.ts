858db38e9e2123edc268a98f1879618aa89e5871
LyingBeagle <alonso.ab.diaz@gmail.com>
2025-04-24 22:18:13 -0400

    Carrusel completado

--- src/app/components/featured-banner/featured-banner.component.ts
+++ src/app/components/featured-banner/featured-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-featured-banner',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./featured-banner.component.scss'],
   standalone:false
 })
-export class FeaturedBannerComponent  implements OnInit {
+export class FeaturedBannerComponent  implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() images: string[] = [
+      'assets/albums/album1.jpg',
+      'assets/albums/album2.jpg',
+      'assets/albums/album3.jpg',
+      'assets/albums/album4.jpg',
+  ];
+  
+  currentIndex = 0;
+  private intervalId!: any;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 5000); //se mueven cada 5 milisegundos
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
 
 }
