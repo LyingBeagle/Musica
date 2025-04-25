7635d326bc0317f78a0eb1bca0b5ce640930f3bf
LyingBeagle <alonso.ab.diaz@gmail.com>
2025-04-24 21:46:09 -0400

    Seccion de discuciones populares completada

--- src/app/components/popular-discussions/popular-discussions.component.ts
+++ src/app/components/popular-discussions/popular-discussions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-popular-discussions',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popular-discussions.component.scss'],
   standalone:false
 })
-export class PopularDiscussionsComponent  implements OnInit {
+export class PopularDiscussionsComponent {
 
-  constructor() { }
-
-  ngOnInit() {}
+  @Input() discussions:{
+    title: string; 
+    excerpt: string; 
+    image: string; 
+  }[] = [{
+      title: 'Mortal Kumbiat',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+      image: 'assets/albums/album4.jpg'
+    },{
+      title: 'Steal this album',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+      image: 'assets/albums/album2.jpg'
+    },{
+      title: 'Forever Howlong',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+      image: 'assets/albums/album1.jpg'
+    },{
+      title: 'I...Am Steve (Bonus Songs from "A Minecraft Movie" Soundtrack)',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+      image: 'assets/albums/album3.jpg'
+    }
+  ];
 
 }
